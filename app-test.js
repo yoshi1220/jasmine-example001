a84f7b0f44f4c656b07f91c24859d158c29859f0
yoshi1220 <temari@themis.ocn.ne.jp>
2019-04-24 23:40:16 +0900

    feat: Implement sortAscending and make test case.

--- app-test.js
+++ app-test.js
@@ -1,23 +1,26 @@
-describe("arithmetic functions", function() {
-  let total;
-
-  beforeEach(function() {
-    total = 100;
-    console.log("beforeEach ran"); // you need to open the console to view this
+describe('arithmetic functions', function() {
+  it('returns the result sorted in ascending order.', function() {
+    const input = '1\n2\n3\n';
+    expect(sortAscending(input)).toEqual([1, 2, 3]);
   });
 
-  it("returns the difference of two numbers", function() {
-    expect(subtract(total, 8)).toBe(92);
+  it('returns the result sorted in ascending order. reversed input.', function() {
+    const input = '3\n2\n1\n';
+    expect(sortAscending(input)).toEqual([1, 2, 3]);
   });
 
-  it("returns the sum of two numbers", function() {
-    expect(add(total, 2)).toBe(102);
+  it('returns the result sorted in ascending order. random input.', function() {
+    const input = '3\n5\n19\n1\n2\n';
+    expect(sortAscending(input)).toEqual([1, 2, 3, 5, 19]);
   });
 
-  //作成分
-  const testCase1 = "3\n5\n19\n1\n2";
+  it('returns the result sorted in ascending order. include same numbers.', function() {
+    const input = '2\n3\n4\n5\n4\n3\n2\n1\n1\n8\n';
+    expect(sortAscending(input)).toEqual([1, 1, 2, 2, 3, 3, 4, 4, 5, 8]);
+  });
 
-  it("testCase1", function() {
-    expect(sortAscending(testCase1)).toBe([1, 2, 3, 5, 19]);
+  it('returns the result sorted in ascending order. all same numbers.', function() {
+    const input = '9\n9\n9\n9\n9\n9\n9\n9\n9\n9\n';
+    expect(sortAscending(input)).toEqual([9, 9, 9, 9, 9, 9, 9, 9, 9, 9]);
   });
 });
--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-function subtract(minued, subtrahend) {
-  return minued - subtrahend;
-}
+/**
+ * 入力された正の整数を昇順にした配列を返す。
+ * 重複する整数があってもそのままとする。
+ * @param {string} input 正の整数を改行で連結した文字列。入力値最終行の末尾に改行が１つ入る。
+ * @return {Array.<number>} 昇順に並んだ正の整数の配列
+ */
+function sortAscending(input) {
+  //改行コードで文字列を分割し、配列に変換する。
+  values = input.trim().split(/\n/);
 
-function add(addend, number) {
-  return addend + number;
-}
+  //文字列の配列を数値の配列に変換する。
+  numbers = values.map(value => parseInt(value));
 
-function sortAscending(positiveIntegersString) {
-  numbers = positiveIntegersString.split(/\n/);
-  console.log(numbers);
-  console.log(numbers);
-  return numbers.sort();
+  //配列を昇順に並び替えた結果を返す。
+  return numbers.sort((a, b) => a - b);
 }
